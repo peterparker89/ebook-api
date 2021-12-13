36c36962c13ba31a5564acd1dbe043fc286867ef
peter <peter.n.m.0123456789@gmail.com>
2021-12-13 17:16:07 +0700

    sanchum

---
+++ routes/api.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\AuthorController;
+use App\Models\Authors;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/ebook','App\Http\Controllers\BookController@index');
+Route::post('/ebook', 'App\Http\Controllers\BookController@create');
+Route::put('/ebook/{id}', 'App\Http\Controllers\BookController@update');
+Route::delete('/ebook/{id}', 'App\Http\Controllers\BookController@delete');
+
+Route::get('/me', [AuthController::class, 'index']);
+
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::post('/authors', [AuthorController::class, 'store']);
+Route::get('authors/{id}', [AuthorController::class, 'show']);
+Route::put('authors/{id}', [AuthorController::class, 'update']);
+Route::delete('authors/{id}', [AuthorController::class, 'destroy']);
+
+Route::post('/login', [LoginController::class, 'store']);
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/login', [LoginController::class, 'create'])->name('login');
+
+Route::post('/store', [LoginController::class, 'store'])->name('store');
+
+Route::get('/user', [LoginController::class, 'user'])->middleware('auth:sanctum');
+
+Route::post('/logout', [LoginController::class, 'destroy'])->name('logout');
